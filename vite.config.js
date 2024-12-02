cdd69f1e5aa0e7d068991be3db7040b6e22ade5a
Jonathan Serafin Contreras Malfavon <167220457+JonathanSerafinCM@users.noreply.github.com>
2024-12-02 12:18:16 -0600

    Arreglar CORS

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,12 @@ export default defineConfig({
             }
         }),
     ],
+    server: {
+        cors: true,
+        port: 5173,
+        strictPort: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
 });
